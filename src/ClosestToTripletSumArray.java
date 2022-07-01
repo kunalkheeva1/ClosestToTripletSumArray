a1e4c755cc0c401c83b7407624912de94afd224e
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-30 21:45:13 -0700

    added comments for the better understanding, I get the whole algorithm, but the surprising part is, i am confused when to take move the pointers in different pointers, in some of the problems we move last, first and middle pointer first, and so on. A part from it, the way mi is being updated is confusing, but I will get it soon, just like all other past problems seem like nothing now, it will happen with this one too, go ahead kunal you can do it, practice, practice, practice.

--- src/ClosestToTripletSumArray.java
+++ src/ClosestToTripletSumArray.java
@@ -1,22 +1,27 @@
 public class ClosestToTripletSumArray {
     public static int closestToTripletSumArray(int arr[], int target){
+        //get three pointers and a max value to store the difference between their sums
         int n = arr.length;
         int i,j,k;
         int mi = Integer.MAX_VALUE;
         int ans_sum = 0;
+
+        // loop, where first pointer stays still and an interior loop runs when they collide then again this loop
         for(i=0; i<n; i++){
             j= i+1;
             k= n-1;
-
+        //until last and middle pointer meets
             while(j<k){
+                //get sum and difference between sum and target
                 int sum = arr[i] +arr[j]+ arr[k];
                 int distance = Math.abs(sum-target);
-
+                // if difference is smaller, then store the sum as and and update mi
                 if(distance <mi){
                     mi = distance;
                     ans_sum = sum;
 
                 }
+                // and move the pointers according to the provided conditions
                 if(sum<target){
                     j++;
                 }else{

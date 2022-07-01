360c41e43ab2829bc9e447d9fdeb4178913e0666
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-30 21:38:21 -0700

    created a method to return the sum of the triplet from the array which is closest to the provided target.

--- src/ClosestToTripletSumArray.java
+++ src/ClosestToTripletSumArray.java
@@ -1,5 +1,31 @@
 public class ClosestToTripletSumArray {
+    public static int closestToTripletSumArray(int arr[], int target){
+        int n = arr.length;
+        int i,j,k;
+        int mi = Integer.MAX_VALUE;
+        int ans_sum = 0;
+        for(i=0; i<n; i++){
+            j= i+1;
+            k= n-1;
+
+            while(j<k){
+                int sum = arr[i] +arr[j]+ arr[k];
+                int distance = Math.abs(sum-target);
+
+                if(distance <mi){
+                    mi = distance;
+                    ans_sum = sum;
+
+                }
+                if(sum<target){
+                    j++;
+                }else{
+                    k--;
+                }
+            }
+        }return ans_sum;
+    }
     public static void main(String[] args) {
-        
+
     }
 }
